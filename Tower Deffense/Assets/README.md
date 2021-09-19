a09f9dbdf2ac6d82b3117342dff1f21c2f13b662
DESKTOP-03VP7M5\user <akuntoramdeku00@gmail.com>
2021-09-19 09:31:35 +0700

    Tugas 4

---
+++ Tower Deffense/Assets/README.md
@@ -0,0 +1,9 @@
+# TowerDefense
+# feature yang ditambahkan antaralain:
+- game pause button
+- screen game resume
+- audio latar belakang
+- adio selesai permainan(menang/kalah)
+
+# Deri Kurniawan
+# 8020190351
---
+++ Tower Deffense/Assets/script/screenPause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenPause : MonoBehaviour
+{
+    //resume game pause
+    [SerializeField]private LevelManager lv;
+   
+    public void resume() 
+    {
+        gameObject.SetActive(false);
+        lv.pausegame = false;
+    }
+}
